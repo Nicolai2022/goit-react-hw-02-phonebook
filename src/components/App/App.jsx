e919261455bbf64ef9b95d3cebac03eb8cee2133
Mykola V <98426734+Nicolai2022@users.noreply.github.com>
2023-04-14 13:54:12 +0300

    add 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,11 +5,10 @@ import { Container, Title, SubTitle } from 'components/App/App.styled';
 class App extends React.Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '222-44-66' },
-      { id: 'id-2', name: 'Hermione Kline', number: '444-88-22' },
-      { id: 'id-3', name: 'Eden Clements', number: '333-00-77' },
-      { id: 'id-4', name: 'Ann Simp', number: '555-91-11' },
-      { id: 'id-4', name: 'Rosa Golovko', number: '123-45-67' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
